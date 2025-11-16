5d4a73f7e4f3c8fb1bd2af64c670984365f31211
alykhns <naliyakhairun@gmail.com>
2025-11-16 12:35:59 +0700

    fix: change path import from path/win32 to path for macOS compatibility

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path/win32';
+import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AuthModule } from './auth/auth.module';
